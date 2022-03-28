9191e8e9618bb79b472ff4f3ab19586de780aa97
AKurosa <98587872+AKurosa@users.noreply.github.com>
2022-03-28 13:22:52 +0900

    Add Ball for sample2.

--- build/forSample2.js
+++ build/forSample2.js
@@ -1,4 +1,6 @@
 let elementOfWave;
+const theUrl = "http://www.w3.org/2000/svg";
+let nC = 0;
 
 var getGetElementForWave = function(id){
     elementOfWave = document.getElementById(id);
@@ -12,4 +14,46 @@ var addWave = function(){
     let dText = `M 140 210 Q 220 ${c}, 300 ${s} T 460 210`
     elementOfWave.setAttribute("d", dText);
     setTimeout(addWave, 100);
+}
+
+let addBallGrad = function(id, color){
+    let stops = [
+        {offset: "20%", stop_color: "white"},
+        {offset: "100%", stop_color: color}
+    ];
+    addGradientRadialDynamic(id, "0.5", "0.5", "1", "0.5", "0.5", stops);
+}
+
+let addBallCore = function(elem, cx, cy, r, color){
+    let c = document.createElementNS(theUrl, "circle");
+    c.setAttribute("cx", cx);
+    c.setAttribute("cy", cy);
+    c.setAttribute("r", r);
+    
+    let id = `cid_${nC++}`;
+    c.setAttribute("id", id);
+
+    elem.prepend(c);
+    addBallGrad(id, color);
+}
+
+var addBalls = function(idSvg){
+    let svg = document.getElementById(idSvg);
+    let stx1 = 180;
+    let sty1 = 250;
+    let colors = ["#fabea7", "#e1eec1", "#aeb5dc",
+                    "#fcc9ac", "#c3dcbe", "#b7aed6",
+                    "#ffe0b6", "#bad4d1", "#c5b2d6",
+                    "#fffac2", "#b4c1d1", "#e5b7be"];
+    let n = 0;
+    for(let i=0; i<4; i++){
+        addBallCore(svg, stx1 + i*70, sty1, 30, colors[n++]);
+    }
+    for(let i=0; i<4; i++){
+        addBallCore(svg, stx1 + 35 + i*70, sty1 + 60, 30, colors[n++]);
+    }
+    for(let i=0; i<4; i++){
+        addBallCore(svg, stx1 + i*70, sty1 + 120, 30, colors[n++]);
+    }
+    
 }
